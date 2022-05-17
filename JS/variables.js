4267b2096ef26858bab7e086f24abb187959dc68
Alistn14 <105198873+Alistn14@users.noreply.github.com>
2022-05-17 14:01:07 +0200

    Empezando JavaScript

---
+++ JS/variables.js
@@ -0,0 +1,21 @@
+const last_name = "last_name";
+const LastName= "LastName";
+const lastName = "lastName";
+document.write(lastName)
+
+const person ="John Doe", carName="Volvo", price=200;
+
+let x; //creando la variable
+x=5; // Asignando valor, de tipo numerico entero y valor 5
+x="Texto";//Asigna valor de tipo texto string
+
+const variable1=1/0;
+
+//NaN
+const variable2=parseFloat('prueba');
+// ejercicio 1 pag 164
+let a=1, b=2, c=3
+console.log(a+b+c)
+
+c=13;
+console.log(a+b+c)

07a3d0a493cedd5c020e9c2a18f18bfbd51ebc64
Alistn14 <105198873+Alistn14@users.noreply.github.com>
2022-05-18 00:31:02 +0200

    repaso java script

--- JS/primerjavascript.js
+++ JS/primerjavascript.js
@@ -1,1 +1,9 @@
 console.log ("Hello world! Texto externo")
+
+     console.log("Hola")
+     console.log('en','un','lugar','de','la','Mancha'); 
+     console.info('Info');
+     console.warn('Warning');  
+     console.error('Error');
+     document.write("Texto2");
+    

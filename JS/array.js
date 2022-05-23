3f46323c4236c5696dccffcdc970bf653fb9f161
Alistn14 <105198873+Alistn14@users.noreply.github.com>
2022-05-23 14:03:00 +0200

    array

---
+++ JS/array.js
@@ -0,0 +1,4 @@
+//Ejercicio1: Escribe un programa que genere un array de 6 valores de tipo number y sean mostrados en pantalla utilizando un bucle for clásico y un bucle for...of
+const numero=[3,5,2,1,4,7];
+
+for (let numero=0; numero<6, numero++);
--- JS/string.js
+++ JS/string.js
@@ -16,3 +16,32 @@ if (string1.charAt(0) == string2.charAt(0) && (string1.charAt(laststring1)== str
 } else {
 document.write("Tienen la primera y ultima letra diferente")
 }
+
+//Ejercicio 3 pág.204 : Escribe un programa que compruebe que el substring "abc" existe en un string dado, pero no puede encontrarse ni al comienzo, ni al final.
+let ejercicio3= "abc lorem abc a abc"
+let existeABC = ejercicio3.indexOf("abc");
+//Cumple que existe del 0 al 14 pero repasemos donde.
+if ((ejercicio3.indexOf("abc")!=-1) && (ejercicio3.indexOf("abc") != 0) && (ejercicio3.lastIndexOf("abc")!= ejercicio3.length-3)) {
+    console.log("Existe ABC dentro del string");
+} else {
+    console.log("No existe ABC dentro");
+} 
+//se podria hacer con slice del 1 al length-2.
+
+//Ejercicio 4: escribe un programa que dado un string y una determinada posición, compruebe que el carácter anterior  y el posterior son iguales o no. REVISAR QUE NO ME SALE
+let stringPos= "Alison Triguero Naranjo";
+let pos= 3
+if (stringPos.charAt(pos) == stringPos.charAt(pos+1)) 
+{
+   console.log("La letra anterior o posterior es igual");
+}
+ else  
+ {
+console.log("No son iguales las letras anterior o posterior");
+ 
+}
+//Ejercicio 7:escribe un programa que elimine el último carácter de un string.
+let text= "Hola buenas";
+text= text.slice(0,-1)
+console.log(text);
+//Desde 0 hasta menos uno (menos la ultima letra)

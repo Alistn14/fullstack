e228228b7f2efb0e252e87ae36683c724b11c182
Alistn14 <alis03tn@gmail.com>
2022-05-27 14:02:31 +0200

    Update main.ts

--- prueba/main.ts
+++ prueba/main.ts
@@ -3,7 +3,9 @@ const saludos = (persona: string) => {
 }
 const usuario: string = "Marcos";
 console.log(usuario);
-document.body.innerHTML = saludos(usuario);
+console.log(saludos(usuario));
+
+$( "button.continue" ).html( "Next Step..." );
 
 //Se ha transcrito el archivo main.ts
 //Transpilación de TypeScript a JavaScript; de main.js a main.ts para ver los errores que se ha producido en java 

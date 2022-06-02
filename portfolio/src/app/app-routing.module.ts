f3e59bdaae86d9394a0c3bc4028661e42c0b33df
Alistn14 <alis03tn@gmail.com>
2022-06-02 14:01:02 +0200

    proyecto portfolio new services and components

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,6 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/core';
-
+// rutas
+import { Routes, RouterModule } from '@angular/router';
+// importaciones
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
+
+const app_routes: Routes=[
+    // ruta principal del local host, cuando la página esta vacía me redirige al portfolio en el local host.
+    { path: "", component: PortfolioComponent}, 
+    // Ruta de la subpágina about (sección) 
+    { path: "equip", component: AboutComponent},
+    // Ruta de la subpágina item (segunda sección)
+    { path: "item", component: ItemComponent},
+// cuando aparece cualquier otra cosa lo redirige al local host otra vez, de vuelta a la página principal.
+    { path: "**", pathMatch:"full" ,redirectTo: ""},
+];
+
+// con forRoot revisa todo el app_routes para reconocer cada ruta.
+@NgModule({
+    imports: [
+        RouterModule.forRoot( app_routes, {useHash:true} )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule { }
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,13 +1,16 @@
+// Módulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
+// Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
-
+// Rutas
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -20,8 +23,11 @@ import { ItemComponent } from './pages/item/item.component';
   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
